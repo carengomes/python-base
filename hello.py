d7fc1f38e5d4d315a610150a1221a1a49ed4e680
Caren Gomes <carengommes@gmail.com>
2025-04-15 12:09:03 -0300

    Adicionando Shebang e comentários

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
+#!/usr/bin/env python3
+
+# acima temos um shebang que indica ao linux/mac qual interpretador do python usar
+# o env é mais flexível, pois determina de acordo com as variáveis do ambiente
+# o windows ignora o shebang, pois ele se baseia na extensão do arquivo
+
 print('Hello, World!')
